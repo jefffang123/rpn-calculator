479c01c0060d37897fc56e4ffd3e66e5d1393026
Jeff Fang <jeff.fang@open.edu.au>
2018-01-25 04:26:03 +1100

    support gradle run

--- src/main/java/com/airwallex/rpn/InsufficientParameterException.java
+++ src/main/java/com/airwallex/rpn/InsufficientParameterException.java
@@ -2,21 +2,7 @@ package com.airwallex.rpn;
 
 public class InsufficientParameterException extends RuntimeException {
 
-    private String operatorValue;
-    private int operatorPosition;
-
-    public String getOperatorValue() {
-        return operatorValue;
-    }
-
-    public int getOperatorPosition() {
-        return operatorPosition;
-    }
-
     public InsufficientParameterException(String operatorValue, int operatorPosition) {
         super(String.format("operator %s (position: %d): insufficient parameters", operatorValue, operatorPosition));
-
-        this.operatorValue = operatorValue;
-        this.operatorPosition = operatorPosition;
     }
 }
