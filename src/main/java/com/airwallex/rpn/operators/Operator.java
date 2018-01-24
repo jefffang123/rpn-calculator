7a5a2c78449cb28f227aaa13aac95881eab9e4e0
Jeff Fang <jeff.fang@open.edu.au>
2018-01-24 22:04:56 +1100

    Operator logics

--- src/main/java/com/airwallex/rpn/operators/Operator.java
+++ src/main/java/com/airwallex/rpn/operators/Operator.java
@@ -4,20 +4,22 @@ import com.airwallex.rpn.Command;
 import com.airwallex.rpn.NumberStack;
 import com.airwallex.rpn.UndoHistory;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 public enum Operator {
 
-    PUSH_NUMBER("", PushNumber::new),
     ADD("+", Add::new),
     SUBTRACT("-", Subtract::new),
     MULTIPLY("*", Multiply::new),
     DIVIDE("/", Divide::new),
     SQRT("sqrt", Sqrt::new),
     UNDO("undo", Undo::new),
-    CLEAR("clear", Clear::new);
+    CLEAR("clear", Clear::new),
+    PUSH_NUMBER(null, PushNumber::new);
 
     private String value;
     private Supplier<? extends NumberStackCommand> supplier;
@@ -38,6 +40,20 @@ public enum Operator {
     }
 
     public static Optional<Operator> findByValue(String value) {
-        return Arrays.stream(Operator.values()).filter(op -> op.value.equalsIgnoreCase(value)).findFirst();
+        Objects.requireNonNull(value);
+
+        return Arrays.stream(Operator.values()).filter(op ->
+                value.equalsIgnoreCase(op.value) || op == PUSH_NUMBER && isValidNumberInput(value)
+        ).findFirst();
+    }
+
+    private static boolean isValidNumberInput(String value) {
+        try {
+            new BigDecimal(value);
+
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
